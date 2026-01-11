5fa06d462c729f2d81039cb6b616ff5801518c0a
linmake <lin.web.05@gmail.com>
2026-01-11 01:57:03 -0700

    add ImageUrl to CarDTOs

--- Backend/DTOs/CarDTOs.cs
+++ Backend/DTOs/CarDTOs.cs
@@ -3,11 +3,15 @@
     public class CarDTOs
     {
         public int Id { get; set; }
-        public decimal Milles { get; set; }
-        public int Year { get; set; }
+
         public int BrandID { get; set; }
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public IFormFile? Image { get; set; }
+
+        public string Model { get; set; } = null!;
+
+        public int Year { get; set; }
+
+        public decimal Milles { get; set; }
+
+        public string? ImageUrl { get; set; }
     }
 }
