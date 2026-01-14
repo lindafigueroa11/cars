dfe1df4a5f8001d6f2bbdd3b3edff9bb8e0da6ef
linmake <lin.web.05@gmail.com>
2026-01-13 17:32:35 -0700

    carresult dtos

--- Backend/Models/Car.cs
+++ Backend/Models/Car.cs
@@ -8,15 +8,24 @@ namespace Backend.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int CarID { get; set; }
-        public string Model { get; set; }
+
+        [Required]
+        public string Model { get; set; } = null!;
+
         public int BrandID { get; set; }
         public int Year { get; set; }
 
         [Column(TypeName = "decimal(18,1)")]
         public decimal Milles { get; set; }
 
-        [ForeignKey("BrandID")]
-        public virtual Brand Brand { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
         public string? ImageUrl { get; set; }
+
+        [ForeignKey("BrandID")]
+        public virtual Brand Brand { get; set; } = null!;
+
+        public virtual CarLocation? Location { get; set; }
     }
 }
--- Backend/Models/CarLocation.cs
+++ Backend/Models/CarLocation.cs
@@ -1,18 +1,24 @@
-﻿namespace Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
 {
     public class CarLocation
     {
-        public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CarLocationID { get; set; }
 
+        [Required]
         public int CarID { get; set; }
+
+        [ForeignKey("CarID")]
         public Car Car { get; set; } = null!;
 
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
-        public string Street { get; set; } = null!;
-        public string StreetNumber { get; set; } = null!;
-        public string Neighborhood { get; set; } = null!;
-        public string City { get; set; } = null!;
+        public string Street { get; set; } = "";
+        public string City { get; set; } = "";
     }
 }
