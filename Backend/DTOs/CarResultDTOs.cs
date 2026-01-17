7495cdd06267d144fed5a4acf7b4bb4d879e4b76
linmake <lin.web.05@gmail.com>
2026-01-17 01:15:31 -0700

    Type transmition manual-automatic

--- Backend/DTOs/CarResultDTOs.cs
+++ Backend/DTOs/CarResultDTOs.cs
@@ -3,20 +3,15 @@
     public class CarResultDTO
     {
         public int CarID { get; set; }
-
-        public string Model { get; set; } = "";
+        public string? Model { get; set; }
         public int Year { get; set; }
-
-        public decimal Milles { get; set; }
-        public decimal Price { get; set; }
-
+        public decimal? Milles { get; set; }
+        public decimal? Price { get; set; }
+        public bool IsAutomatic { get; set; }
         public string? ImageUrl { get; set; }
-
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-
-        public string Street { get; set; } = "";
-        public string City { get; set; } = "";
+        public string? Street { get; set; }
+        public string? City { get; set; }
     }
-
 }
